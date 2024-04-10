f264b1c57086afad3cf6adf9f001cc3211286021
Gabriel Caique <112442554+APBielzinx@users.noreply.github.com>
2024-04-10 13:02:49 -0300

    Clone instagram

---
+++ React/clone-instagram/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import ConteudoLateralEsquerda from './componentes/ConteudoLateralEsquerda';
+
+function App() {
+  return (
+    <>
+     <ConteudoLateralEsquerda/>
+      <div className='conteudoCentral'>
+
+      </div>
+
+      <div className='conteudoLateralDireita'>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ React/clone-instagram/src/componentes/ConteudoLateralEsquerda.js
@@ -0,0 +1,34 @@
+import logo from '../assets/imagens/logo.png'
+
+export default function ConteudoLateralEsquerda() {
+   return(
+    <>
+    <div className='conteudoLateralEsquerda'>
+        <div className='logoInstagram'>
+            <img src={logo} alt='Logo instagram'></img>
+        </div>
+        <div className='menu'>
+            <div className='meuIcone'>
+                Home
+            </div>
+            <div className='meuIcone'>
+            Home
+            </div>
+            <div className='meuIcone'>
+            Home
+            </div>
+            <div className='meuIcone'>
+            Home
+            </div>
+            <div className='meuIcone'>
+            Home
+            </div>
+            <div className='meuIcone'>
+            Home
+            </div>
+        </div>
+    </div>
+</>
+   )
+
+}
